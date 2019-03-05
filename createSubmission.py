a913af06d604dbeaedcbf329888082a5482a3ed6
Pinardy <pinardy@hotmail.com>
2019-03-05 22:36:32 +0800

    Added create submission script

--- createSubmission.py
+++ createSubmission.py
@@ -5,6 +5,20 @@ def getFeatures(data):
     features = data.drop(columns=['itemid', 'title', 'image_path'])
     return list(features)
 
+def createSubmissionDataframe(predictions, features):
+    columns = ['id','tagging']
+    rows = []
+
+    for index, data in predictions.iterrows():
+        for feature in features:
+            id_label = str(data['itemid']) + "_" + feature
+            row = [id_label, data[feature][1] + " " + data[feature][4]]
+            rows.append(row)
+
+    
+    submission_df = pd.DataFrame(rows, columns=columns) 
+    return submission_df
+
 
 ######################################################################
 
@@ -14,5 +28,14 @@ if __name__ == "__main__":
     fashion_data_predictions = pd.read_csv('predictions/fashion_data_info_val_prediction_competition.csv', encoding='utf8')
     beauty_data_predictions = pd.read_csv('predictions/beauty_data_info_val_prediction_competition.csv', encoding='utf8')
 
-    #TODO: Create submission file with the saved prediction files
+    # Create the individual submission dataframes
+    mobile_submission_df = createSubmissionDataframe(mobile_data_predictions, getFeatures(mobile_data_predictions))
+    fashion_submission_df = createSubmissionDataframe(fashion_data_predictions, getFeatures(fashion_data_predictions))
+    beauty_submission_df = createSubmissionDataframe(beauty_data_predictions, getFeatures(beauty_data_predictions))
 
+    # Combine the submission dataframes into one
+    submission_df = pd.concat([mobile_submission_df, fashion_submission_df, beauty_submission_df])
+    submission_df.to_csv("predictions/data_info_val_submission.csv", index=False)
+    print("Submission file created")
+
+    
